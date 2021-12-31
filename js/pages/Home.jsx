7fc3407fabee385ab3407a8acddba2fb83770c95
Itamar Avady <itamaravady@gmail.com>
2021-12-31 19:44:59 +0200

    change hero

--- js/pages/Home.jsx
+++ js/pages/Home.jsx
@@ -3,11 +3,12 @@ const { NavLink } = ReactRouterDOM;
 export function Home() {
     return (
         <div className="home">
-            <h2>Welcome to the World's finest Appsus!</h2>
+            <h2>Welcome to Appsus.</h2>
+
             <div className="home-links-container">
-                <NavLink className="clean-link" to="/mail/"> <img src="../../assets/img/mail.png" /></NavLink>
-                <NavLink className="clean-link" to="/note/"> <img src="../../assets/img/notes.png" /></NavLink>
-                <NavLink className="clean-link" to="/book/"> <img src="../../assets/img/books.png" /></NavLink>
+                <NavLink className="clean-link" to="/mail/"> <img src="../../assets/img/mail-home.png" /></NavLink>
+                <NavLink className="clean-link" to="/note/"> <img src="../../assets/img/notes-home.png" /></NavLink>
+                <NavLink className="clean-link" to="/book/"> <img src="../../assets/img/book-home.png" /></NavLink>
             </div>
         </div>
     )
