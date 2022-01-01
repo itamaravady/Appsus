ab6c55da8c709b90978e192d557c01afb577a21c
Itamar Avady <itamaravady@gmail.com>
2022-01-01 21:32:34 +0200

    ff

---
+++ js/pages/About.jsx
@@ -0,0 +1,12 @@
+
+
+export function About() {
+    return (
+        <div className="about">
+            <h1>We are Ben & Itamar </h1>
+            <h3>Appsus is & Notes app (and books...)</h3>
+            <p>We're glad to have you here :) come check it out</p>
+            <img src="assets/img/zoom.png" alt="" />
+        </div >
+    )
+}
